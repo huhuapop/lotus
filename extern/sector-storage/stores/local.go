1e75c1eacc63239c1ee702e2ab01cebc0eec9cae
huhuafupop <huhuapop@outlook.com>
2021-08-25 20:03:28 -0400

    不检查磁盘by默然代码

--- extern/sector-storage/stores/local.go
+++ extern/sector-storage/stores/local.go
@@ -388,7 +388,8 @@ func (st *Local) Reserve(ctx context.Context, sid storage.SectorRef, ft storifac
 		overhead := int64(overheadTab[fileType]) * int64(ssize) / storiface.FSOverheadDen
 
 		if stat.Available < overhead {
-			return nil, storiface.Err(storiface.ErrTempAllocateSpace, xerrors.Errorf("can't reserve %d bytes in '%s' (id:%s), only %d available", overhead, p.local, id, stat.Available))
+			// 根据默然代码更改 先不使用
+			//return nil, storiface.Err(storiface.ErrTempAllocateSpace, xerrors.Errorf("can't reserve %d bytes in '%s' (id:%s), only %d available", overhead, p.local, id, stat.Available))
 		}
 
 		p.reserved += overhead
