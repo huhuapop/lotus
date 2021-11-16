9599f2e78d83ebdab67e98a43d3d3627c04cfb61
huafu <huhuapop@outlook.com>
2021-11-16 08:04:13 -0800

    add LOTUS_WDPOST && LOTUS_WNPOST && workername

--- node/modules/storageminer.go
+++ node/modules/storageminer.go
@@ -250,7 +250,12 @@ func StorageMiner(fc config.MinerFeeConfig) func(params StorageMinerParams) (*st
 
 		lc.Append(fx.Hook{
 			OnStart: func(context.Context) error {
-				go fps.Run(ctx)
+				// go fps.Run(ctx)
+				if _, ok := os.LookupEnv("LOTUS_WDPOST"); ok {
+					go fps.Run(ctx)
+				} else {
+					log.Warnf("This miner will be disable windowPoSt.")
+				}
 				return sm.Run(ctx)
 			},
 			OnStop: sm.Stop,
