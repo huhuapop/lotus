9dbf5a813736d325470d7079e384a1856e7f420e
huhuafupop <huhuapop@outlook.com>
2022-04-18 11:02:19 -0400

    update storiface.ID

--- extern/sector-storage/selector_existing.go
+++ extern/sector-storage/selector_existing.go
@@ -86,7 +86,7 @@ func (s *existingSelector) FindDataWoker(ctx context.Context, task sealtasks.Tas
 		return false
 	}
 
-	have := map[stores.ID]struct{}{}
+	have := map[storiface.ID]struct{}{}
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
 	}
