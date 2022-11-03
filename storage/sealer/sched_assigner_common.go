5df68029cbdfcb0ce90d2552cea5a38e4aec7570
huafu <huhuapop@outlook.com>
2022-11-03 08:42:19 -0700

    update DefaultDefaultMaxFee from 0.07 to 0.27

--- storage/sealer/sched_assigner_common.go
+++ storage/sealer/sched_assigner_common.go
@@ -148,7 +148,7 @@ func (a *AssignerCommon) TrySched(sh *Scheduler) {
 
 	wg.Wait()
 
-	log.Debugf("SCHED windows: %+v", windows)
+	// log.Debugf("SCHED windows: %+v", windows)
 	log.Debugf("SCHED Acceptable win: %+v", acceptableWindows)
 
 	// Step 2
