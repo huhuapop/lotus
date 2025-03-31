a27235b13f6c7ded79b70909308d3760bab86634
huhuapop <huhuapop@outlook.com>
2025-03-31 13:07:22 -0700

    add LOTUS_WDPOST && LOTUS_WNPOST && workername

--- build/version.go
+++ build/version.go
@@ -18,7 +18,7 @@ func NodeUserVersion() BuildVersion {
 }
 
 // MinerBuildVersion is the local build version of the Lotus miner
-const MinerBuildVersion = "1.32.1"
+const MinerBuildVersion = "1.32.1-huafu"
 
 func MinerUserVersion() BuildVersion {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
