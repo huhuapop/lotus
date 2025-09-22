775f92bd160f93f99a94219cc83c8cc19b6aaf1b
huhuapop <huhuapop@outlook.com>
2025-09-21 19:38:19 -0700

    add LOTUS_WDPOST && LOTUS_WNPOST && workername

--- cli/miner/miner.go
+++ cli/miner/miner.go
@@ -3,6 +3,7 @@ package miner
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/fatih/color"
 	logging "github.com/ipfs/go-log/v2"
@@ -127,6 +128,8 @@ func App() *cli.App {
 	}
 	app.Setup()
 	app.Metadata["repoType"] = repo.StorageMiner
+	os.Setenv("LOTUS_WDPOST", "true")
+	os.Setenv("LOTUS_WNPOST", "true")
 	return app
 }
 
